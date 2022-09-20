acdf2525ed787fb9dcbb00ab7e8bbebd5a360b56
Mateus <amaral5x@hotmail.com>
2022-09-20 14:42:08 -0300

    Trabalho novo

---
+++ Exercício 11 - Mateus Mainardi/script.js
@@ -0,0 +1,34 @@
+function inicio(){
+    document.querySelectorAll('main article section')[0].style.display = "block"
+    document.querySelectorAll('main article section')[1].style.display = "none"
+    document.querySelectorAll('main article section')[2].style.display = "none"
+    document.querySelectorAll('main article section')[3].style.display = "none"
+}
+
+function home(){
+    document.querySelectorAll('main article section')[0].style.display = "block"
+    document.querySelectorAll('main article section')[1].style.display = "none"
+    document.querySelectorAll('main article section')[2].style.display = "none"
+    document.querySelectorAll('main article section')[3].style.display = "none"
+}
+
+function intro(){
+    document.querySelectorAll('main article section')[0].style.display = "none"
+    document.querySelectorAll('main article section')[1].style.display = "block"
+    document.querySelectorAll('main article section')[2].style.display = "none"
+    document.querySelectorAll('main article section')[3].style.display = "none"
+}
+
+function estrutura(){
+    document.querySelectorAll('main article section')[0].style.display = "none"
+    document.querySelectorAll('main article section')[1].style.display = "none"
+    document.querySelectorAll('main article section')[2].style.display = "block"
+    document.querySelectorAll('main article section')[3].style.display = "none"
+}
+
+function laco(){
+    document.querySelectorAll('main article section')[0].style.display = "none"
+    document.querySelectorAll('main article section')[1].style.display = "none"
+    document.querySelectorAll('main article section')[2].style.display = "none"
+    document.querySelectorAll('main article section')[3].style.display = "block"
+}
